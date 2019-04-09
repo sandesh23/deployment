e784d39f088ad5e9c1362c2c889cd210cbc97e3d
Sandesh Mahamure <s.mahamure@gmail.com>
2019-04-09 15:10:38 +0530

    one line removed

--- hotelapp/models.py
+++ hotelapp/models.py
@@ -27,7 +27,7 @@ class Hotel(models.Model):
     balance = models.IntegerField()
     costToBusiness= models.IntegerField()
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
-'''This is new line'''
+
 '''This line is added by another developer'''
 class Waiter(models.Model):
     aadharNo = models.BigIntegerField(primary_key=True)
